cd2620163dee4c4ffceb28960726c1b0d7dee4d2
Rakibul-98 <rakibul.rupom2001@gmail.com>
2023-07-31 05:22:17 +0600

    component structure done

---
+++ src/Home/components/Footer/SocialBtn/SocialBtn.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SocialBtn = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default SocialBtn;
---
+++ src/Home/components/Projects/ProjctsMenu/ProjectsMenu.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProjectsMenu = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default ProjectsMenu;
