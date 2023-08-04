4afece18e64857b08fc5cec78f3026c4552b1682
Rakibul-98 <rakibul.rupom2001@gmail.com>
2023-08-04 06:11:30 +0600

    banner responsiveness done

--- src/Home/components/Banner/Banner.js
+++ src/Home/components/Banner/Banner.js
@@ -3,12 +3,12 @@ import BannerInfo from '../Shared/BannerInfo/BannerInfo';
 
 const Banner = () => {
     return (
-        <div className='md:flex w-10/12 mx-auto items-center'>
-            <div className='md:w-5/12'>
-                <BannerInfo/>
+        <div className='w-10/12 md:w-10/12 md:flex flex-row-reverse mx-auto items-center py-20'>
+            <div className='pb-5'>
+                <img className='scale-90 hover:scale-100 duration-1000 md:scale-110' src="https://i.ibb.co/Sr3MzK7/laptop.png" alt="laptop" />
             </div>
-            <div className=''>
-                <img className='' src="https://i.ibb.co/Sr3MzK7/laptop.png" alt="laptop" />
+            <div className='md:w-11/12 lg:w-6/12'>
+                <BannerInfo/>
             </div>
         </div>
     );
--- src/Home/components/Shared/BannerInfo/BannerInfo.js
+++ src/Home/components/Shared/BannerInfo/BannerInfo.js
@@ -4,9 +4,9 @@ const BannerInfo = () => {
     return (
         <div>
             <p className='uppercase font-bold'>- welcome to logo design</p>
-            <h2 className='uppercase text-3xl font-bold md:w-8/12'>advance business solution <span className='text-blue-600'>ux ui design</span></h2>
+            <h2 className='uppercase text-3xl font-bold w-9/12 sm:w-8/12 md:w-11/12 xl:w-8/12'>advance business solution <span className='text-blue-600'>ux ui design</span></h2>
             <p className='my-5'><small>we are dedicated to shaping exceptional digital experiences that propel businesses to new heights. As a leading digital agency, we combine creativity, innovation, and cutting-edge technology to craft captivating digital solutions that leave a lasting impact.</small></p>
-            <button className='px-8 py-3 bg-blue-600 rounded-full uppercase text-sm text-white cursor-pointer hover:bg-blue-900'>explore our services</button>
+            <button className='px-8 py-3 bg-blue-600 rounded-full uppercase text-sm text-white cursor-pointer hover:bg-blue-900 duration-500'>explore our services</button>
         </div>
     );
 };
