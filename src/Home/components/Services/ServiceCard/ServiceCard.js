20256c790d3d1113396bbf2ae057b05b1836426a
Rakibul-98 <rakibul.rupom2001@gmail.com>
2023-08-04 07:58:49 +0600

    service sec complete

--- src/Home/components/Services/ServiceCard/ServiceCard.js
+++ src/Home/components/Services/ServiceCard/ServiceCard.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
-const ServiceCard = () => {
+const ServiceCard = ({service}) => {
     return (
-        <div>
-            
+        <div className={`${service.class} px-2 lg:px-12 py-5`}>
+            {
+                service.img?<img className='lg:h-28 lg:w-28 h-20 w-20 mx-auto p-2' src={service.img} alt="" /> : <p className='font-bold uppercase'>{service.motto}</p>
+            }
+            {
+                service.title ? <h3 className='text-center uppercase font-semibold pb-3'>{service.title}</h3> : <h3 className='font-bold text-4xl uppercase py-3'>our <span className='text-blue-600'>services.</span></h3>
+            }
+            {
+                service.text ? <p className='text-center text-xs md:text-base'>{service.text.slice(0,250)}. . .</p> : <p className='text-xs md:text-base'>Services are intangible and can cover a wide range of activities across various industries. They are distinct from physical goods, as they involve the provision of expertise, skills, or labor to deliver value to customers or clients.</p>
+            }
         </div>
     );
 };
--- src/Home/components/Services/Services.js
+++ src/Home/components/Services/Services.js
@@ -1,37 +1,25 @@
 import React from 'react';
+import ServiceCard from './ServiceCard/ServiceCard';
 
 const Services = () => {
+
+    const services = [
+        { id: 1, class: "bg-gray-200 card hover:bg-blue-500 hover:shadow-blue-500 hover:shadow-md hover:text-white", img: "https://i.ibb.co/z4YdGSf/logo.png", title: "logo design", text: "Logo design is the process of creating a visual representation that symbolizes a brand, company, organization, or individual. A logo is a distinctive and recognizable graphic mark, emblem, or symbol that serves as a visual representation of the entity it represents. Logos are often used to convey the essence, values, and identity of a brand in a concise and memorable way." },
+
+        { id: 2, class: "bg-gray-200 card hover:bg-blue-500 hover:shadow-blue-500 hover:shadow-md hover:text-white", img: "https://i.ibb.co/j6nPhCg/multimedia.png", title: "multimedia", text: "Multimedia design involves the creation and integration of various forms of media, such as text, images, audio, video, animations, and interactive elements, to communicate messages and information in a dynamic and engaging manner. It is a multidisciplinary field that combines elements of graphic design, animation, audio production, video editing, and interactive design to create cohesive and impactful multimedia experiences." },
+        { id: 3, motto: "-what we do" },
+
+        { id: 4, class: "bg-gray-200 card hover:bg-blue-500 hover:shadow-blue-500 hover:shadow-md hover:text-white", img: "https://i.ibb.co/b2F7BBY/info.png", title: "infographic", text: "An infographic is a visual representation of information, data, or knowledge presented in a concise and engaging format. It combines text, images, charts, and diagrams to convey complex concepts or data in a more easily understandable and visually appealing manner. Infographics are designed to quickly communicate information and insights, making them particularly useful for presenting statistics, trends, comparisons, processes, or explanations." },
+        { id: 5, class: "bg-gray-200 card hover:bg-blue-500 hover:shadow-blue-500 hover:shadow-md hover:text-white", img: "https://i.ibb.co/8zyGy4c/print.png", title: "print design", text: "Print design refers to the process of creating visual materials that are intended to be printed or physically produced, such as brochures, flyers, posters, business cards, magazines, packaging, and other promotional materials. It involves designing layouts, selecting typography, arranging images, and creating graphics with the goal of producing visually appealing and effective printed materials." },
+        { id: 6, class: "bg-gray-200 card hover:bg-blue-500 hover:shadow-blue-500 hover:shadow-md hover:text-white", img: "https://i.ibb.co/YQx3Bd7/web-design.png", title: "web design", text: "Web design refers to the process of creating and designing the visual layout, aesthetics, and user experience of websites. It involves combining various elements, including layout, colors, typography, graphics, images, and interactive features, to produce a visually appealing and functional website." }
+    ]
+
     return (
-        <div className='grid grid-cols-3 gap-5'>
-            <div className="card border p-2">
-                <img src="" alt="" />
-                <h3></h3>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem vel accusantium alias voluptate veniam laudantium exercitationem hic dolorum itaque eaque.</p>
-            </div>
-            <div className="card border p-2">
-                <img src="" alt="" />
-                <h3></h3>
-                <p>Quaerat, soluta dignissimos asperiores iusto labore ipsa vitae nulla commodi. Molestiae soluta totam nam reprehenderit esse inventore unde placeat dicta!</p>
-            </div>
-            <div className="card border p-2">
-                <img src="" alt="" />
-                <h3></h3>
-                <p>Facere libero neque eligendi eaque voluptatibus vitae velit minima ipsam officia delectus molestiae rem ipsum fugiat, iusto ullam! Veritatis, quae?</p>
-            </div>
-            <div className="card border p-2">
-                <img src="" alt="" />
-                <h3></h3>
-                <p>Inventore et pariatur soluta possimus sed ipsam cupiditate neque voluptates iure quas enim, ea alias dignissimos fugit sint, corrupti illum.</p>
-            </div>
-            <div className="card border p-2">
-                <img src="" alt="" />
-                <h3></h3>
-                <p>Expedita blanditiis unde animi eligendi veniam ut, deleniti dicta recusandae quasi, doloremque aspernatur nesciunt! Fuga delectus molestias architecto eligendi cum.</p>
-            </div>
-            <div className="card border p-2">
-                <img src="" alt="" />
-                <h3></h3>
-                <p>Magni illum quia commodi eum libero velit corporis soluta voluptatem asperiores, accusantium pariatur, ducimus temporibus odit voluptatibus vitae dolorem cumque!</p>
+        <div className="bg-[url('https://i.ibb.co/xJCsmq2/bg1.png')] bg-no-repeat bg-right-top">
+            <div className='w-10/12 mx-auto grid grid-cols-2 md:grid-cols-3 gap-5 xl:gap-12 my-10'>
+                {
+                    services.map(service => <ServiceCard key={service.id} service={service}></ServiceCard>)
+                }
             </div>
         </div>
     );
